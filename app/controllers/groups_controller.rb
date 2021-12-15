5eb777a520c06e58fdc993a7628be918c3133d34
Ismail Karabulut <i.karabulut93@gmail.com>
2021-12-15 13:47:50 -0600

    adds standard RESTful routes to Groups controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,43 @@
 class GroupsController < ApplicationController
+  ### Standard RESTful routes ###
+  def create
+    group = Group.new(
+      user_id: current_user.id,
+      name: params["name"],
+      email: params["email"],
+      address: params["address"],
+      website: params["website"],
+      mission_statement: params["mission_statement"]
+    )
+    if group.save
+      render json: group.as_json
+    else
+      render json: {errors: product.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  def index
+    groups = Group.all
+    render json: groups.as_json
+  end
+
+  def update
+    group = Group.find_by(id: params["id"])
+    group.name = params["name"] || group.name
+    group.email = params["email"] || group.email
+    group.address = params["address"] || group.address
+    group.website = params["website"] || group.website
+    group.mission_statement = params["mission_statement"] || group.mission_statement
+    if group.save
+      render json: {message: "Group successfully updated", params: group.as_json}
+    else
+      render json: {errors: group.errors.full_messages}, status: 418
+    end
+  end
+
+  def show
+    group = Group.find_by(id: params["id"])
+    render json: group.as_json
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-### USER ROUTES
+### User Routes
   post "/users" => "users#create"
-### SESSION ROUTES
+
+### Session Routes
   post "/sessions" => "sessions#create"
+
+### Group Routes
+  post "/groups" => "groups#create"
+  get "/groups" => "groups#index"
+  put "/groups/:id" => "groups#update"
+  get "/groups/:id" => "groups#show"
 end
