ce84d7733b9ebb2a4a61899e57d6a397554dce92
Ismail Karabulut <i.karabulut93@gmail.com>
2021-12-21 20:34:42 -0600

    adds image_url and description to groups serializer

--- app/serializers/group_serializer.rb
+++ app/serializers/group_serializer.rb
@@ -1,5 +1,5 @@
 class GroupSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :name, :email, :address, :website, :mission_statement
+  attributes :id, :user_id, :name, :email, :address, :website, :mission_statement, :image_url, :description
   belongs_to :user
   has_many :events
   has_many :group_causes
