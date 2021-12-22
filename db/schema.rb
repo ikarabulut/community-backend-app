e2834da42199543c897f940dec639f44cb20dfe0
Ismail Karabulut <i.karabulut93@gmail.com>
2021-12-21 20:32:38 -0600

    adds description column to groups

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_22_022739) do
+ActiveRecord::Schema.define(version: 2021_12_22_023215) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 2021_12_22_022739) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "image_url"
+    t.text "description"
   end
 
   create_table "rsvps", force: :cascade do |t|
