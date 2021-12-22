e4c268da097b0cf60f733714ae731afb55a8f197
Ismail Karabulut <i.karabulut93@gmail.com>
2021-12-21 21:52:56 -0600

    updates event_tags create method to pull in tag name as a param

--- app/controllers/event_tags_controller.rb
+++ app/controllers/event_tags_controller.rb
@@ -1,9 +1,10 @@
 class EventTagsController < ApplicationController
   def create
     event = Event.find_by(id: params["event_id"])
+    tag = Tag.find_by(tag: params["tag"])
     event_tag = EventTag.new(
       event_id: event.id,
-      tag_id: params["tag_id"]
+      tag_id: tag.id
     )
     if event_tag.save
       render json: event_tag
