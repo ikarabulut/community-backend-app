629ebc975a74c49a869f5e487b9fa2a97f778ac0
Ismail Karabulut <i.karabulut93@gmail.com>
2021-12-27 11:45:54 -0600

    updates cors origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://still-citadel-91201.herokuapp.com/'
+    origins '*'
 
     resource '*',
       headers: :any,
